322430fc0678f5f500f7f0f24b6cdbcbee58e8a9
ttaylor <troytaylor.net@gmail.com>
2017-06-20 15:42:13 -0400

    SelectMany - Compound from 1

--- LinqSamples.cs
+++ LinqSamples.cs
@@ -301,12 +301,24 @@ namespace SampleQueries
             }
         }
 
-        [Category("")]
-        [Title("")]
+        [Category("Projection Operators")]
+        [Title("SelectMany - Compound from 1")]
         [Description("")]
         public void Linq14()
         {
+            int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            int[] numbersB = { 1, 3, 5, 7, 8 };
 
+            var pairs = from a in numbersA
+                        from b in numbersB
+                        where a < b
+                        select new { a, b };
+
+            Console.WriteLine("Pairs where a < b:");
+            foreach (var pair in pairs)
+            {
+                Console.WriteLine("{0} is less than {1}", pair.a, pair.b);
+            }
         }
 
         [Category("")]
